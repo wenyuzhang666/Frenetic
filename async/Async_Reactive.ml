b51777ec8d77a0e94821dd51d171c3be73440d6e
Spiros Eliopoulos <seliopou@gmail.com>
2014-01-17 15:16:10 -0500

    reactive: factor out state/transitions into a module
    Separate the model loop from state updates.

--- async/Async_Reactive.ml
+++ async/Async_Reactive.ml
@@ -75,46 +75,75 @@ let choose_policy t pols =
       | `Ok local -> Some(local) in
   choice op (fun e -> `Policy(e))
 
-type ('e, 'p) state = {
+module State = struct
+
+  type ('e, 'p) t = {
     local : VInt.t -> SDN_Types.flowTable;
     sws : (Int64.t * PortSet.t) SwitchMap.t;
     e : 'e Deferred.choice option;
     p : 'p Deferred.choice option
-}
+  }
+
+  let create local = {
+    local;
+    sws = SwitchMap.empty;
+    e = None;
+    p = None
+  }
+
+  let add_switch s ~c_id ~feats =
+    let open OF0x01 in
+    let sw_id = feats.SwitchFeatures.switch_id in
+    let ports = PortSet.of_list
+      (List.filter_map feats.SwitchFeatures.ports ~f:(fun p ->
+        if PortDescription.(p.config.PortConfig.down)
+          then None
+          else Some(VInt.Int16(p.PortDescription.port_no)))) in
+    ({ s with sws = SwitchMap.add s.sws ~key:c_id ~data:(sw_id, ports) },
+     (sw_id, ports))
+
+  let remove_switch s ~c_id =
+    { s with sws = SwitchMap.remove s.sws c_id }
+
+  let add_port s ~c_id ~desc =
+    { s with
+      sws = let (sw_id, ports) = SwitchMap.find_exn s.sws c_id in
+        SwitchMap.add s.sws ~key:c_id ~data:(sw_id, PortSet.add ports
+          (VInt.Int16(OF0x01.PortDescription.(desc.port_no))))
+    }
+
+  let remove_port s ~c_id ~desc =
+    { s with
+      sws = let (sw_id, ports) = SwitchMap.find_exn s.sws c_id in
+        SwitchMap.add s.sws ~key:c_id ~data:(sw_id, PortSet.remove ports
+          (VInt.Int16(OF0x01.PortDescription.(desc.port_no))))
+    }
+
+end
 
 let start ~f ~port ~init_pol ~pols =
   Controller.create ~port () >>= function t ->
   Log.info "Listening for switches";
   let evts = T.run Controller.features t (Controller.listen t) in
-  let state = {
-    local = f init_pol;
-    sws = SwitchMap.empty;
-    e = Some(choose_event t evts);
-    p = Some(choose_policy t pols)
+  let state = { (State.create (f init_pol)) with
+    State.e = Some(choose_event t evts);
+    State.p = Some(choose_policy t pols)
   } in
 
-  Deferred.forever state (fun ({ local; sws; e; p} as s) ->
-    Deferred.choose (filter_map (fun e -> e) [ e ; p ])
+  Deferred.forever state (fun s ->
+    Deferred.choose (filter_map (fun e -> e) [ s.State.e ; s.State.p ])
     >>= function
       | `Event Some(evt) ->
         begin match evt with
-          | `Connect(c_id, feat) ->
-            let open OF0x01 in
-            let sw_id = feat.SwitchFeatures.switch_id in
-            let ports = PortSet.of_list
-              (List.filter_map feat.SwitchFeatures.ports ~f:(fun p ->
-                if PortDescription.(p.config.PortConfig.down)
-                  then None
-                  else Some(VInt.Int16(p.PortDescription.port_no)))) in
-            Deferred.all (to_messages (local (VInt.Int64 sw_id)) ports ~f:(Controller.send t c_id))
-            >>| fun _ -> { s with
-              sws = SwitchMap.add sws ~key:c_id ~data:(sw_id, ports);
-              e = Some(choose_event t evts)
+          | `Connect(c_id, feats) ->
+            let s', (sw_id, ports) = State.add_switch s c_id feats in
+            let local_pol = s.State.local (VInt.Int64 sw_id) in
+            Deferred.all (to_messages local_pol ports ~f:(Controller.send t c_id))
+            >>| fun _ -> { s' with
+              State.e = Some(choose_event t evts)
             }
           | `Disconnect(c_id, _) ->
-            return { s with
-              sws = SwitchMap.remove sws c_id
-            }
+            return (State.remove_switch s c_id)
           | `Message(c_id, msg) ->
             failwith "NYI"
         end
@@ -124,10 +153,10 @@ let start ~f ~port ~init_pol ~pols =
           let sw_id, ports = data in
           Deferred.all (to_messages (local (VInt.Int64 sw_id)) ports ~f:(Controller.send t key))
           >>| (fun _ -> ()) in
-        Deferred.Map.iter sws ~f:next
+        Deferred.Map.iter s.State.sws ~f:next
         >>= fun _ -> return { s with
-          local = local;
-          p = Some(choose_policy t pols)
+          State.local = local;
+          State.p = Some(choose_policy t pols)
         }
       | `Event None ->
         return { state with e = None }

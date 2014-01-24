fb6b2200b1004893d30691646f8daae095cccc8a
Arjun Guha <arjun@cs.umass.edu>
2014-01-24 15:08:44 -0500

    Made "dist" a sub-command of katnetic

---
+++ lib/DistributedCompiler.mli
@@ -0,0 +1,35 @@
+(** Distributed compiler for Frenetic *)
+
+open Core.Std
+open Async.Std
+
+(**
+
+ An example configuration:
+
+{
+  workers: [ "10.152.136.136" ]
+  per_worker: 10;
+}
+
+*)
+type config = {
+  workers: string list;   (* hostnames or IP addresses of worker machines  *)
+  per_worker: int;        (* number of jobs to run per worker *)
+}
+
+(** Parses a configuration from a file *)
+val parse_config : string -> config
+
+
+(** Runs the distributed compiler. Debugging output prints to stderr and
+    benchmarking output prints to stdout. The computation returns when the
+    compilation completes. Right now, the actual policy is not returned.
+
+    Assumes that [Parallel.init] has already been invoked and that all workers
+    are part of the cluster. *)
+val dist_compiler : Types.policy -> 
+  min_sw:int ->
+  max_sw:int ->
+  config:config ->
+  unit Deferred.t

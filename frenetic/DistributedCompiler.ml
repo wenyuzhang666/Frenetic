9435ddd374b489b191329263e62205f15ad4190d
Arjun Guha <arjun@cs.umass.edu>
2014-01-24 12:21:20 -0500

    deleting remote stuff

--- frenetic/DistributedCompiler.ml
+++ frenetic/DistributedCompiler.ml
@@ -41,9 +41,9 @@ let measure_time label (f : unit -> 'a Deferred.t) : 'a Deferred.t =
   return r
 
 let cluster_map (lst : 'a list) 
-                ~(per_worker : int)
-               ~(config : config) 
-               ~(f : 'a -> 'b Deferred.t) : ('a, exn) Result.t list Deferred.t =
+  ~(per_worker : int)
+  ~(config : config) 
+  ~(f : string -> 'a -> 'b Deferred.t) : ('a, exn) Result.t list Deferred.t =
   let (workers_r, workers_w) = Pipe.create () in
   let workers = List.concat_map (range 1 per_worker) 
                   ~f:(fun _ -> config.workers) in
@@ -55,7 +55,7 @@ let cluster_map (lst : 'a list)
     | `Eof -> failwith "unexpected EOF from cluster_map worker pipe"
     | `Ok worker -> 
        try_with
-         (fun () -> Parallel.run ~where:(`On worker) (fun () -> f x))
+         (fun () -> Parallel.run ~where:(`On worker) (fun () -> f worker x))
        >>= (function
        | Error exn -> 
          p (sprintf "Exception from %s: %s" worker (Exn.to_string exn));
@@ -89,11 +89,14 @@ let parse_caching (src : string) (dump : string) : Types.policy Deferred.t =
          pol)
 
 (* Runs on a worker to receive a policy and dump it to disk. *)
-let receive_policy (pol_file : string) (pol_data : string) () : unit Deferred.t = 
-  Writer.with_file pol_file (fun w ->
-    Writer.write w pol_data;
-    p "received policy";
-    return ())
+let receive_policy (pol_data : string) () : string Deferred.t = 
+  let (tmp_path, out_chan) = Filename.open_temp_file "frenetic" "kat" in
+  let writer = Writer.of_out_channel out_chan Fd.Kind.File in
+  Writer.write writer pol_data;
+  Writer.close writer >>=
+  fun () ->
+  (* Closing out_chan appears to be unnecessary *)
+  return tmp_path
 
 let compile_in_process ~pol_dump ~sw =
   p (sprintf "starting compile ~sw:%d ~pol:_" sw);
@@ -114,14 +117,18 @@ let compile ~pol_dump ~sw =
   measure_time (sprintf "compile ~sw:%d" sw) (fun () ->
     compile_in_process ~pol_dump ~sw)
 
-let rec ship_policy (pol_file : string)
+let rec ship_policy tbl
                     (pol_data : string) 
                     (worker : string) : unit Deferred.t =
   p (sprintf "Sending policy to %s" worker);
   try_with (fun () ->
-    Parallel.run ~where:(`On worker) (receive_policy pol_file pol_data)
+    Parallel.run ~where:(`On worker) (receive_policy pol_data)
     >>= function
-    | Ok r -> p (sprintf "ship_policy to %s succeeded" worker); return ()
+    | Ok remote_file -> 
+      p (sprintf "ship_policy to %s succeeded" worker);
+      (* Returned result indicates if it is a duplicate entry. *)
+      let _ = Hashtbl.add tbl ~key:worker ~data:remote_file in
+      return ()
     | Error e -> p (sprintf "ship_policy to %s died with %s" worker e); return ())
     >>| function
     | Ok x -> x
@@ -129,31 +136,7 @@ let rec ship_policy (pol_file : string)
       (p (sprintf "exception in ship_policy to %s; %s" worker (Exn.to_string exn));
        ())
 
-let rec reader config =
-  printf "> %!";
-  Reader.read_line (force Reader.stdin)
-  >>= function
-  | `Eof -> Shutdown.shutdown 0; return ()
-  | `Ok input ->
-  let parsed_input = String.split input ~on:' ' in
-  try_with (fun () ->
-    match parsed_input with
-    | [ "ship"; filename ] -> 
-      ship config filename
-    | [ "compile"; filename; per_worker; m; n ] ->
-      ((try return (Some (Int.of_string m, Int.of_string n)) with _ -> return None)
-       >>= function
-       | None -> return ()
-       | Some (m, n) -> 
-         compile_all config filename (Int.of_string per_worker) m n)
-    | _ -> return ())
-  >>= function
-  | Ok () -> reader config
-  | Error exn ->
-    printf "Exception:\n%s\n%!" (Exn.to_string exn);
-    reader config
-
-and ship config filename =
+and ship config tbl filename =
   measure_time "shipping data" (fun () ->
     let src = filename ^ ".kat" in
     let bin = filename ^ ".bin" in
@@ -161,32 +144,54 @@ and ship config filename =
     >>= fun _ ->
     Reader.file_contents bin
     >>= fun bin_data ->
-    Deferred.List.iter ~how:`Parallel ~f:(ship_policy bin bin_data) config.workers)
+    Deferred.List.iter ~how:`Parallel ~f:(ship_policy tbl bin_data) config.workers)
 
-and compile_all config filename (per_worker : int) min_sw max_sw =
-  let bin = filename ^ ".bin" in
+and compile_all config cached_policies (per_worker : int) min_sw max_sw =
   let switches = range min_sw max_sw in
   measure_time "compilation"
     (fun () -> 
-       cluster_map switches ~config ~per_worker ~f:(fun sw -> compile ~pol_dump:bin ~sw))
+       cluster_map switches ~config ~per_worker 
+         ~f:(fun worker sw ->
+               let pol_dump = Hashtbl.find_exn cached_policies worker in
+               compile ~pol_dump ~sw))
   >>= fun results ->
   let failures = List.filter_map results ~f:Result.error in
   List.iter failures ~f:(fun exn -> p (Exn.to_string exn));
   printf "%d failures.\n%!" (List.length failures);
   return ()
 
+let rm_tmp cached_policies worker = 
+  Parallel.run ~where:(`On worker)
+    (fun () -> Unix.remove (Hashtbl.find_exn cached_policies worker))
+  >>= function
+    | Ok () -> return ()
+    | Error exn ->
+      (p (sprintf "rm_tmp exception: %s" exn));
+      return ()
+
 let main () = 
   match Array.to_list Sys.argv with
-    | [_; "local"; pol_dump; n] ->
-      let sw = Int.of_string n in
-      let _ = compile_in_process ~pol_dump ~sw in
-      never_returns (Scheduler.go ())
-    | [_; "master"; filename ] ->
-      let config = parse_config filename in
+    | [_; "master"; config_file; pol_file; per_worker; min_sw; max_sw ] ->
+      let config = parse_config config_file in
+      let per_worker = Int.of_string per_worker in
+      let min_sw = Int.of_string min_sw in
+      let max_sw = Int.of_string max_sw in
       Parallel.init ~cluster: { Cluster.master_machine = Unix.gethostname();
                                 worker_machines = config.workers } ();
+      (* Map from remote hostname to remote filename. *)
+      let cached_policies = String.Table.create () in
+      let ship_and_compile () = 
+        ship config cached_policies pol_file 
+        >>= fun () ->
+        compile_all config cached_policies per_worker min_sw max_sw in
+      let finally_block () = 
+        Deferred.List.iter config.workers
+          ~how:`Parallel ~f:(rm_tmp cached_policies) in
+      let _ =
+        Monitor.protect ship_and_compile finally_block
+        >>= fun () ->
+        Shutdown.exit 0 in
       p "Master started";
-      let _ = reader config in
       never_returns (Scheduler.go ())
     | [_] -> Parallel.init ()
     | _ -> printf "Invalid argument. See source code for help.\n%!"
